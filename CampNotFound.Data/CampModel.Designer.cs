e5a525502c0b53a19c66370bfdf88365340f0999
Ivan Grgurina <ivan.grgurina@gmail.com>
2017-04-09 05:19:42 +0200

    Added description to events

--- CampNotFound.Data/CampModel.Designer.cs
+++ CampNotFound.Data/CampModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\marko.raguz\Documents\GitHub\camp-not-found\CampNotFound.Data\CampModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Ivan Grgurina\Source\Repos\camp-not-found\CampNotFound.Data\CampModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- CampNotFound.Web/Controllers/EventController.cs
+++ CampNotFound.Web/Controllers/EventController.cs
@@ -44,6 +44,7 @@ namespace CampNotFound.Web.Controllers
         // GET: Event/Create
         public ActionResult Create()
         {
+            ViewBag.MovieType = GetCurrencyDropdown();
             return View();
         }
 
@@ -62,6 +63,22 @@ namespace CampNotFound.Web.Controllers
             }
 
             return View(@event);
+        }
+
+        private List<SelectListItem> GetCurrencyDropdown()
+        {
+            List<SelectListItem> items = new List<SelectListItem>();
+
+            items.Add(new SelectListItem { Text = "Action", Value = "0" });
+
+            items.Add(new SelectListItem { Text = "Drama", Value = "1" });
+
+            items.Add(new SelectListItem { Text = "Comedy", Value = "2", Selected = true });
+
+            items.Add(new SelectListItem { Text = "Science Fiction", Value = "3" });
+
+            //ViewBag.MovieType = items;
+            return items;
         }
 
         // GET: Event/Edit/5
